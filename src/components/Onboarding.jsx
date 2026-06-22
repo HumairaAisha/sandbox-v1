0964d9faedb8e08883a99911a99a08579e6738a2
USER <aishamuarin@gmail.com>
2026-06-22 06:51:56 +0100

    Add content text on mobile view

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -30,10 +30,19 @@ function Onboarding() {
   return (
     <div className="min-h-screen px-8 py-4">
        <div className="flex flex-col justify-between">
-         <button onClick={() => navigate("/home")} className="md:flex text-2xl font-semibold cursor-pointer md:mt-4">Sandbox</button>
+         <button onClick={() => navigate("/home")} className="text-3xl font-semibold mt-4 text-center md:text-left cursor-pointer">Sandbox</button>
 
       <div className="max-w-7xl mx-auto">
-         <div className="py-20 md:py-4 px-10 grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-20 items-center min-h-[80vh]">
+         <div className="py-8 md:py-4 px-0 sm:px-6 grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 items-center min-h-[80vh]">
+            <div className="flex lg:hidden flex-col text-center mb-4">
+        <h2 className="font-semibold text-xl text-sandbox-navy mb-4">
+          Build Your Growth Intentionally
+        </h2>
+        <p className="text-gray-500 text-sm">
+          Track your progress, log challenges, and reflect on the journey.
+        </p>
+      </div>
+
             <div className="hidden lg:flex flex-col justify-center">
             <h2 className="font-semibold text-5xl leading-tight mb-6 text-sandbox-navy">
               Build Your <br /> Growth Intentionally
@@ -46,7 +55,7 @@ function Onboarding() {
 
 
    <div className="flex justify-center lg:justify-start">
-      <div className="w-full shadow-xl rounded-3xl px-8 py-10">
+      <div className="w-full max-w-md shadow-xl rounded-3xl px-6 sm:px-8 py-8 bg-white">
          <MyForm onSubmit={handleSumbit}>
          <InputField
          name={"name"}
@@ -60,7 +69,7 @@ function Onboarding() {
          required={true}
          placeholder="e.g Frontend Developer, Fullstack Engineer"
          />
-      <button type="submit" className="bg-sandbox-navy text-white rounded-2xl w-full py-3 font-medium hover:bg-sandbox-navy/90 transition-colors hover:cursor-pointer">
+      <button type="submit" className="bg-sandbox-navy text-white rounded-2xl w-full py-4 font-medium hover:bg-sandbox-navy/90 transition-colors hover:cursor-pointer">
       Go to Dashboard
       </button>
 
